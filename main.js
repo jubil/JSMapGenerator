23bcd19ca47ffd581b5998d3b838cf90356def78
Valentin GENEVRAIS <valentin.genevrais@cbp-group.com>
2024-10-25 18:45:08 +0200

    voronoi with d3js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-json = {
+/*json = {
     "points": [
         { "x": 0, "y": 0 },
         { "x": 0, "y": 100 },
@@ -24,9 +24,9 @@ json = {
             "points": "50,50 25,25 10,20, 40,10"
         }
     ]
-}
+}*/
 
-function loadGraph() {
+function loadGraph(json) {
     console.log("DEBUG JSON", json)
 
     json.points.forEach(p => {
@@ -55,4 +55,37 @@ function loadGraph() {
 
     })
 
+}
+
+function generateJsonRandom() {
+    json = JSON.parse("{}")
+    //json.points = [{x:10, y: 25}]
+    //json.edges = [{path:[{x:10, y: 25}, {x:10, y: 30}, {x:20, y: 25}]}]
+    //json.polygon = [{points:"50,50 25,25 10,20, 40,10"}]
+
+    json.points = []
+    json.edges = []
+    json.polygon = []
+
+    for (let i = 0; i < 1000; i++) {
+        json.points.push({x:Math.floor(Math.random() * 500 + 250),y:Math.floor(Math.random() * 500 + 250)})
+    }
+    
+    //console.log("points", json.points.map(p => [p.x, p.y]))
+    //const delaunay = d3.Delaunay.from([[0, 0], [0, 100], [100, 0], [100, 100]]);
+    
+    const delaunay = d3.Delaunay.from(json.points.map(p => [p.x, p.y]));
+    //console.log("delaunay", delaunay)
+    //const voronoi = delaunay.voronoi([-10000, -10000, 10000, 10000]);
+    const voronoi = delaunay.voronoi([100, 100, 900, 900]);
+    //console.log("voronoi", voronoi)
+    
+    console.log("voronoi cells", voronoi.cellPolygons())
+    voronoi.cellPolygons().forEach(cell => {
+        json.polygon.push({"points": JSON.stringify(cell)
+        .replaceAll("],[", " ")
+        .replaceAll("[[", "")
+        .replaceAll("]]", "")})
+    })
+    return json
 }
