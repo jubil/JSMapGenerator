34e108f5579ba03812438d3c498ff6afaa66265e
Valentin GENEVRAIS <valentin.genevrais@cbp-group.com>
2024-10-25 15:25:43 +0200

    V2 polygon

--- main.js
+++ main.js
@@ -6,16 +6,12 @@ json = {
         { "x": 100, "y": 100 },
         { "x": 50, "y": 50 }
     ],
-    "edges": [
+    "polygon": [
         {
-            "path": [
-                { "x": 50, "y": 50 },
-                { "x": 60, "y": 60 },
-                { "x": 87, "y": 58 },
-                { "x": 98, "y": 86 },
-                { "x": 100, "y": 100 }
-            ]
-        },
+            "id":"TODO id",
+            "metadata": "TODO metadata",
+            "points": "50,50 25,25 10,20, 40,10"
+        }
     ]
 }
 
@@ -30,26 +26,13 @@ function loadGraph() {
         document.getElementById("graph-big-points").appendChild(node)
     })
 
-    json.edges.forEach(e => {
-        // TODO Path
-        let edge = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-        let path = "M" + e.path[0].x + "," + e.path[0].y + " ";
-        e.path.slice(1).forEach(p => {
-            path += "L" + p.x +"," + p.y + " ";
-        })
-        edge.setAttribute("d", path);
+    json.polygon.forEach(p => {
+        let tile = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
+        tile.setAttribute("points", p.points)
+        //tile.setAttribute("stroke", "#F55")
+        tile.setAttribute("fill", "#"+Math.floor(Math.random()*16777215).toString(16))
+        document.getElementById("graph-tiles").appendChild(tile)
 
-        // 
-        e.path.slice(1, e.path.length - 1).forEach(p => {
-            let edgeNode = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
-            edgeNode.setAttribute("cx", p.x)
-            edgeNode.setAttribute("cy", p.y)
-            edgeNode.setAttribute("r", 1)
-            document.getElementById("graph-small-points").appendChild(edgeNode)
-        })
-
-
-        document.getElementById("edge").appendChild(edge)
     })
 
 }
