4b6d5df4c576e9f61f7e168809b544cd6d1d5cf5
Valentin GENEVRAIS <valentin.genevrais@gmail.com>
2024-12-02 03:38:33 +0100

    roads pathfinding Dijkstra

--- TestNoise/world.js
+++ TestNoise/world.js
@@ -157,41 +157,6 @@ function generateJsonRandom() {
         .replaceAll("]]", ""),
       sommets: cell.slice(),
     });
-
-    // TODO A Suppr
-    // Générer des routes sur toutes les tuiles de plaines
-/*     if (biome == 20) {
-      for (let i = 0; i < cell.length - 1; i++) {
-        const sommetA = cell[i];
-        const sommetB = cell[i + 1];
-
-        // TODO Si pas besoin de détour (les 2 centre de tuiles en ligne droite, pas besoin de courbe)
-        //Si besoin de faire un détour
-        let dest = [
-          sommetA[0] + 0.5 * (sommetB[0] - sommetA[0]),
-          sommetA[1] + 0.5 * (sommetB[1] - sommetA[1]),
-        ];
-
-        let p2 = [
-          centre[0] + 0.2 * (dest[0] - centre[0]),
-          centre[1] + 0.2 * (dest[1] - centre[1]),
-        ];
-
-        let p3;
-        if (isVertical(sommetA, sommetB)) {
-          p3 = [centre[0] + 0.3 * (dest[0] - centre[0]), dest[1]];
-        } else {
-          p3 = [dest[0], centre[1] + 0.3 * (dest[1] - centre[1])];
-        }
-
-        json.routes.push({
-          p1: centre,
-          p2: p2,
-          p3: p3,
-          p4: dest,
-        });
-      }
-    } */
   });
 
   // Calcul la taille des tuiles
@@ -200,15 +165,15 @@ function generateJsonRandom() {
   });
 
   // Force borders (0 = Océan || 10 = Montagne)
-  // forceBorders(0);
+  forceBorders(0);
 
   // Calcul voisin
   json.tiles.forEach((t) => {
     t.neighbors = Array.from(voronoi.neighbors(t.id)).map((id) => {
       let voisin = json.tiles.filter((t2) => t2.id == id)[0];
 
-      let segmentSeparation = voisin.sommets.flatMap(sommetVoisin => {
-        return t.sommets.map(sommetTuile => {
+      let segmentSeparation = voisin.sommets.slice(0, voisin.sommets.length-1).flatMap(sommetVoisin => {
+        return t.sommets.slice(0, t.sommets.length-1).map(sommetTuile => {
           return {
             s1: sommetTuile,
             s2: sommetVoisin,
@@ -261,6 +226,51 @@ function generateJsonRandom() {
   tuileDeVillePotentielles.forEach((t) => {
     t.color = "lime";
   });
+
+  // DEBUG créer quelques routes pour relier les villages
+  /*for (let i = 0; i < tuileDeVillePotentielles.length; i++) {
+    createLongRoad(pathfinding(tuileDeVillePotentielles[i].id, tuileDeVillePotentielles[(i+1)%tuileDeVillePotentielles.length].id, [20] ));
+  }*/
+
+  // Relier tous les villages
+  /* tuileDeVillePotentielles.forEach(v1 => {
+    tuileDeVillePotentielles.forEach(v2 => {
+      if(v1 != v2){
+        createLongRoad(pathfinding(v1.id, v2.id, [20]))
+      }
+    })
+  })*/
+
+  // DEBUG Chaque village est relié à ses 3 plus proches voisins
+  tuileDeVillePotentielles.map((t) => {
+    return {
+      src: t.id,
+      centre: t.centre,
+      dest: tuileDeVillePotentielles
+        .filter((t2) => t2.id != t.id)
+        .map((t2) => {
+          return {
+            id: t2.id,
+            dist: calculDistance(
+              t.centre[0],
+              t.centre[1],
+              t2.centre[0],
+              t2.centre[1]
+            ),
+          };
+        })
+        .sort((d1, d2) => {
+          return d1.dist - d2.dist;
+        }).slice(0, 3),
+    };
+  }).map(t => {
+    return {src: t.src, dest: t.dest.flatMap(d => d.id)}
+  }).forEach(road => {
+    road.dest.forEach(dest => {
+      createLongRoad(pathfinding(road.src, dest, [20]))
+    })
+  });
+  //
 
   return json;
 }
@@ -347,6 +357,11 @@ function generateColorFromBiomeId(biomeId) {
 }
 
 function createSemiRoad(idTuile, idVoisin) {
+  if(!json.tiles.filter((t) => t.id == idTuile)[0].neighbors.filter((n) => n.id == idVoisin)[0]){
+    console.error("Les tuiles ne sont pas voisines", idTuile, idVoisin)
+    return;
+  }
+
   const centre = json.tiles.filter((t) => t.id == idTuile)[0].centre;
   const dest = json.tiles.filter((t) => t.id == idTuile)[0].neighbors.filter((n) => n.id == idVoisin)[0].centreSegmentSeparation;
 
@@ -364,6 +379,11 @@ function createSemiRoad(idTuile, idVoisin) {
     p3 = [dest[0], centre[1] + 0.3 * (dest[1] - centre[1])];
   }
 
+  // Vérifier si la route existe déjà pour ne pas créer de doublons
+  if(json.routes.filter(r => r.p1 == centre && r.p4 == dest).length > 0){
+    return;
+  }
+
   json.routes.push({
     p1: centre,
     p2: p2,
@@ -377,8 +397,10 @@ function createRoad(idTuile, idVoisin){
   createSemiRoad(idVoisin, idTuile)
 }
 
-
-function createLongRoad(...idTuiles) {
+function createLongRoad(idTuiles) {
+  if(!idTuiles || idTuiles.length == 0){
+    return;
+  }
   for (let index = 0; index < idTuiles.length-1; index++) {
     createRoad(idTuiles[index], idTuiles[index+1])
   }
@@ -546,3 +568,70 @@ function cleanGraphSVG() {
   document.getElementById("graph-points").innerHTML = "";
 }
 
+// PATHFINDING
+// TODO A SUPPR
+function pathfinding_old(idTuileDebut, idTuileFin) {
+  const m = json.tiles.filter(t => t.id == idTuileFin)[0].centre
+  let path = [idTuileDebut], i = idTuileDebut, c;
+
+  while ((c = delaunay._step(i, ...m)) >= 0 && c !== i && c !== idTuileDebut) {
+    path.push(i = c);
+  }
+
+  return path;
+}
+
+//TODO (Dijkstra)
+function pathfinding(idTuileDebut, idTuileFin, biomesAutorises) {
+  if(!biomesAutorises){
+    biomesAutorises = [10, 20, 21]
+  }
+  
+  let debut = json.tiles.filter(t => t.id == idTuileDebut)[0];
+  let fin = json.tiles.filter(t => t.id == idTuileFin)[0];
+
+  const data = json.tiles.map((t) => {
+    return { id: t.id, dist: Infinity, prev: undefined, closed: false };
+  });
+
+  let current = debut;
+  data.filter(d => d.id == debut.id)[0].dist = 0;
+  data.filter(d => d.id == debut.id)[0].closed = true;
+
+  while(current.id != fin.id){
+    // récupérer voisins autorisés
+    let voisinsPossibles = json.tiles.filter(t => t.id == current.id)[0].neighbors.filter(n => biomesAutorises.includes(n.biome))
+    voisinsPossibles.forEach((v) => {
+      if(data.filter(d => d.id == current.id)[0].dist + v.distance < data.filter(d => d.id == v.id)[0].dist){
+        // Nouveau plus cour chemin pour aller en v depuis current
+        data.filter(d => d.id == v.id)[0].prev = current.id;
+        data.filter(d => d.id == v.id)[0].dist = data.filter(d => d.id ==current.id)[0].dist + v.distance;
+      }
+    })
+    // Si tous les chemins bloqués
+    if(data.filter(d => !d.closed && d.dist != Infinity).length == 0){
+      // TODO Dans ce cas, ? créer des ports pour laisser un moyen de traverser la mer / ? voyages rapides 
+      console.error("Chemins bloqués pathfinding", idTuileDebut, idTuileFin, biomesAutorises)
+      return null;
+    }
+
+    // Choisir noeud suivant à bloquer
+    let idSuivant = data.filter(d => !d.closed && d.dist != Infinity).sort((d1, d2) => {return d1.dist - d2.dist})[0].id
+    data.filter(d => d.id == idSuivant)[0].closed = true
+    current = json.tiles.filter(t => t.id == idSuivant)[0]
+  }
+  
+  // Reconstruire le chemin
+  let path = [];
+  let c = current.id;
+  while (c != debut.id) {
+    if (!c) {
+      console.error("c undefined")
+    }
+    path.unshift(c);
+    c = data.filter(d => d.id == c)[0].prev
+  }
+  path.unshift(debut.id);
+  return path
+
+}
