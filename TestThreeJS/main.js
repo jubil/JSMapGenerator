29ba4888cd04357185d58e0d9d13068b2d391cd1
Valentin GENEVRAIS <valentin.genevrais@gmail.com>
2024-12-10 21:30:16 +0100

    format

--- TestThreeJS/main.js
+++ TestThreeJS/main.js
@@ -1,6 +1,6 @@
 import * as THREE from "../libs/three/three.module.js";
 import { VOXLoader, VOXMesh } from "../libs/three/addons/loaders/VOXLoader.js";
-import { Sky } from '../libs/three/addons/objects/Sky.js';
+import { Sky } from "../libs/three/addons/objects/Sky.js";
 
 let FOV = 80;
 
@@ -20,18 +20,18 @@ document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-const texture = new THREE.TextureLoader().load( "texture.png" );
+const texture = new THREE.TextureLoader().load("texture.png");
 texture.wrapS = THREE.RepeatWrapping;
 texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set( 4, 4 );
+texture.repeat.set(4, 4);
 
 const material = new THREE.MeshBasicMaterial({
   map: texture,
 });
 const cube = new THREE.Mesh(geometry, material);
-cube.position.x = 5
-cube.position.y = 6
-cube.position.z = 5
+cube.position.x = 5;
+cube.position.y = 6;
+cube.position.z = 5;
 scene.add(cube);
 
 let cube2 = new THREE.Mesh(
@@ -65,71 +65,74 @@ loader.load("1.vox", function (chunks) {
     meshVOX = new VOXMesh(chunk);
     meshVOX.scale.setScalar(1);
     meshVOX.position.z = -10;
-    meshVOX.position.y = chunk.size.y/2-1
+    meshVOX.position.y = chunk.size.y / 2 - 1;
 
-    console.log(chunk)
-    console.log(meshVOX)
+    console.log(chunk);
+    console.log(meshVOX);
 
     scene.add(meshVOX);
   }
 });
 
 // sky
-const textureCiel = new THREE.TextureLoader().load( "sky.jpg" );
+const textureCiel = new THREE.TextureLoader().load("sky.jpg");
 textureCiel.wrapS = THREE.RepeatWrapping;
 textureCiel.wrapT = THREE.RepeatWrapping;
-textureCiel.repeat.set( 1, 1 );
+textureCiel.repeat.set(1, 1);
 const sphereCiel = new THREE.Mesh(
   new THREE.SphereGeometry(900, 100, 100),
   new THREE.MeshBasicMaterial({
     map: textureCiel,
-    side: THREE.BackSide
+    side: THREE.BackSide,
   })
 );
 scene.add(sphereCiel);
 
 // helpers
-scene.add( new THREE.GridHelper( 100, 100 ) );
+scene.add(new THREE.GridHelper(100, 100));
 //scene.add( new THREE.AxesHelper( 5 ) );
-
 
 // render
 
 camera.position.x = -10;
-camera.position.y = 10
+camera.position.y = 10;
 camera.position.z = 5;
 let i = 0;
 function animate() {
-  i++
+  i++;
   renderer.render(scene, camera);
   cube.rotation.x += 0.001;
   cube.rotation.y += 0.01;
   //camera.rotateY(-0.001)
 
-  sphereCiel.position.x = camera.position.x
-sphereCiel.position.y = camera.position.y
-sphereCiel.position.z = camera.position.z
+  sphereCiel.position.x = camera.position.x;
+  sphereCiel.position.y = camera.position.y;
+  sphereCiel.position.z = camera.position.z;
 
-  if(meshVOX && i % 200 == 0){
+  if (meshVOX && i % 200 == 0) {
     i = 0;
     meshVOX.material.wireframe = !meshVOX.material.wireframe;
   }
-
 }
 renderer.setAnimationLoop(animate);
 
-document.addEventListener("keypress", event => {
-  if(event.key == 'z'){
-    camera.position.z -= 1
-  }if(event.key == 'q'){
-    camera.position.x -= 1
-  }if(event.key == 's'){
-    camera.position.z += 1
-  }if(event.key == 'd'){
-    camera.position.x += 1
-  }if(event.key == 'e'){
-    camera.rotation.y -= Math.PI/64
-  }if(event.key == 'a'){
-    camera.rotation.y += Math.PI/64
+document.addEventListener("keypress", (event) => {
+  if (event.key == "z") {
+    camera.position.z -= 1;
   }
-})
+  if (event.key == "q") {
+    camera.position.x -= 1;
+  }
+  if (event.key == "s") {
+    camera.position.z += 1;
+  }
+  if (event.key == "d") {
+    camera.position.x += 1;
+  }
+  if (event.key == "e") {
+    camera.rotation.y -= Math.PI / 64;
+  }
+  if (event.key == "a") {
+    camera.rotation.y += Math.PI / 64;
+  }
+});
